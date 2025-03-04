41578d6999415ca874b4a953c537209640659fe1
Andy Yu <piratestt@gmail.com>
2025-03-04 10:16:31 -0500

    Revamp lab home page

--- main.js
+++ main.js
@@ -11,9 +11,13 @@ function setTitle() {
     const backgroundHeight = titleImg.offsetHeight;
 
     const h1Offset = (backgroundHeight - navHeight) / 2 + navHeight - 20;
+    const pOffset = (backgroundHeight - navHeight) + 0;
 
     const titleH1 = titleImg.querySelector('h1');
     titleH1.style.top = `${h1Offset}px`;
+
+    // const titleP = titleImg.querySelector('p');
+    // titleP.style.top = `${pOffset}px`;
 }
 
 document.addEventListener('DOMContentLoaded', setTitle);
